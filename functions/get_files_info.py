9be58b27e23a3e71447c9eb1cad13135162ec00d
johannesalke <johannesalke@gmail.com>
2026-03-20 22:07:03 +0100

    Implemented function to get info on directory contents.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,4 +8,26 @@ def get_files_info(working_directory, directory="."):
 
     if valid_target_dir == False:
         return f'Error: Cannot list "{target_dir}" as it is outside the permitted working directory'
-    
+    if os.path.isdir(target_dir) == False:
+        return f'Error: "{directory}" is not a directory'
+
+    dirItemList = os.listdir(target_dir)
+    #print(dirItemList)
+
+
+    result = "Results for the current directory:\n"
+    for item in dirItemList:
+        #print(item)
+        itemPath = os.path.join(target_dir,item)
+        #print(itemPath)
+        is_dir = os.path.isdir(itemPath)
+        file_size = os.path.getsize(itemPath)
+        result += f"  - {item}: file_size={file_size} bytes, is_dir={is_dir}\n"
+            
+    return result
+
+
+
+        
+
+
