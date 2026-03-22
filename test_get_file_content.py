b3141a5c0dc537b3370ab552b0241961d686d5e5
johannesalke <johannesalke@gmail.com>
2026-03-22 15:58:16 +0100

    restored testsuit

--- test_get_file_content.py
+++ test_get_file_content.py
@@ -4,13 +4,13 @@ from functions.get_file_content import get_file_content
 
 def test():
     result = get_file_content("calculator", "main.py")
-    print("def main():")
+    print(result)
 
     result = get_file_content("calculator", "pkg/calculator.py")
-    print("def _apply_operator(self, operators, values)")
+    print(result)
 
     result = get_file_content("calculator", "/bin/cat")
-    print("Error:")
+    print(result)
 
 
 if __name__ == "__main__":
